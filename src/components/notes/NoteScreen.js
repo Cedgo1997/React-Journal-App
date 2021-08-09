cf348ad61044b1f54c905cb5bb8d1d2604ba9a22
César Gonzalez <cedgo1997@gmail.com>
2021-08-09 19:56:08 -0400

    notes styles

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,24 @@ export const NoteScreen = () => {
 	return (
 		<div className='notes__main-content'>
 			<NotesAppBar />
+			<div className='notes__content'>
+				<input
+					type='text'
+					placeholder='Some awesome title'
+                    className='notes__title-input'
+                    autoComplete='off'
+				/>
+				<textarea
+					placeholder='What happened today'
+					className='notes__textarea'
+				></textarea>
+				<div className='notes__image'>
+					<img
+						src='https://media.cdnandroid.com/26/0e/f3/c8/imagen-the-spider-man-homecoming-app-0thumb.jpeg'
+						alt='pic'
+					/>
+				</div>
+			</div>
 		</div>
 	);
 };
