600295e9b743ea9efe0e7b9d05fd02f7fc13a777
César Gonzalez <cedgo1997@gmail.com>
2021-08-10 17:09:59 -0400

    Actions added and useDispatch

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { login } from '../../actions/auth';
 import { useForm } from '../../hooks/useForm.';
 
 export const LoginScreen = () => {
@@ -8,9 +10,11 @@ export const LoginScreen = () => {
 		password: '',
 	});
 
+	const dispatch = useDispatch();
+
 	const handleLogin = (e) => {
 		e.preventDefault();
-		console.log(email, password);
+		dispatch(login(123, 'César'));
 		reset();
 	};
 
