6e2303970a8af489905ef80a5c88f54ee89ff92c
César Gonzalez <cedgo1997@gmail.com>
2021-08-10 21:47:15 -0400

    Validations

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from '../../hooks/useForm.';
+import validator from 'validator';
 
 export const RegisterScreen = () => {
 	const [{ name, email, password, password2 }, handleInputChange, reset] =
@@ -13,16 +14,27 @@ export const RegisterScreen = () => {
 
 	const handleRegister = (e) => {
 		e.preventDefault();
-		console.log(name, email, password, password2);
-		reset();
+		if (isFormValid()) {
+			console.log(name, email, password, password2);
+			reset();
+		}
 	};
 
-	const isFormValid = () => {};
+	const isFormValid = () => {
+		if (name.trim().length === 0) {
+			return false;
+		} else if (!validator.isEmail(email)) {
+			return false;
+		} else if (password !== password2 || password.length < 5) {
+			return false;
+		}
+	};
 
 	return (
 		<>
 			<h3 className='auth__title'>Register</h3>
 			<form onSubmit={handleRegister}>
+				<div className='auth__alert-error'>Hola mundo</div>
 				<input
 					className='auth__input'
 					type='text'
