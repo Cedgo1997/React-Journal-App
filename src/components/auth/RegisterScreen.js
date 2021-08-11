a7a057a2bc423147d154c31fb8b5eca8a88f93b9
César Gonzalez <cedgo1997@gmail.com>
2021-08-10 21:11:24 -0400

    extracting data from register screen

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm.';
 
 export const RegisterScreen = () => {
+	const [{ name, email, password, password2 }, handleInputChange, reset] =
+		useForm({
+			name: '',
+			email: '',
+			password: '',
+			password2: '',
+		});
+
+	const handleRegister = (e) => {
+		e.preventDefault();
+		console.log(name, email, password, password2);
+		reset();
+	};
+
+	const isFormValid = () => {};
+
 	return (
 		<>
 			<h3 className='auth__title'>Register</h3>
-			<form>
+			<form onSubmit={handleRegister}>
 				<input
 					className='auth__input'
 					type='text'
 					placeholder='Name'
 					name='name'
+					value={name}
+					onChange={handleInputChange}
 					autoComplete='off'
 				/>
 				<input
@@ -18,6 +37,8 @@ export const RegisterScreen = () => {
 					type='text'
 					placeholder='Email'
 					name='email'
+					value={email}
+					onChange={handleInputChange}
 					autoComplete='off'
 				/>
 				<input
@@ -25,12 +46,16 @@ export const RegisterScreen = () => {
 					type='password'
 					placeholder='Password'
 					name='password'
+					onChange={handleInputChange}
+					value={password}
 				/>
 				<input
 					className='auth__input'
 					type='password'
 					placeholder='Confirm Password'
-					name='password'
+					name='password2'
+					onChange={handleInputChange}
+					value={password2}
 				/>
 				<button
 					className='btn btn-primary btn-block mt-1'
