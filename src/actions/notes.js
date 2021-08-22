aabbfff42165c90fea23fd53a072615b3a9ccd12
César Gonzalez <cedgo1997@gmail.com>
2021-08-22 15:32:01 -0400

    display newnotes in real time

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -14,6 +14,7 @@ export const startNewNote = () => {
 		};
 		const doc = await db.collection(`${uid}/journal/notes`).add(newNote);
 		dispatch(activeNote(doc.id, newNote));
+		dispatch(addNewNote(doc.id, newNote));
 	};
 };
 
@@ -24,6 +25,13 @@ export const activeNote = (id, note) => ({
 		...note,
 	},
 });
+
+export const addNewNote = (id, note) => ({
+	type: types.addNewNote,
+	payload: {
+		id, ...note
+	}
+})
 
 export const startLoadingNotes = (uid) => {
 	return async (dispatch) => {
